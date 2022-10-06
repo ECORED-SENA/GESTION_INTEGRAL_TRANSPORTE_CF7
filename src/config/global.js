b3e2ee8a0536dd97a5acc3afe374f1e40ee52fbc
DiegoVelasco84 <83100265+DiegoVelasco84@users.noreply.github.com>
2022-10-06 06:49:05 -0500

    Avance 4

--- src/config/global.js
+++ src/config/global.js
@@ -95,11 +95,6 @@ export default {
         titulo: 'Glosario',
       },
       {
-        nombreRuta: 'complementario',
-        icono: 'far fa-folder-open',
-        titulo: 'Material complementario',
-      },
-      {
         icono: 'fas fa-book',
         titulo: 'Referencias bibliográficas',
         nombreRuta: 'referencias',
