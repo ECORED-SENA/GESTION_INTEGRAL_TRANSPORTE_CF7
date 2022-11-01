9635982b9c56ba51cd2b1feb636274126c4b00fb
DiegoVelasco84 <83100265+DiegoVelasco84@users.noreply.github.com>
2022-11-01 10:22:10 -0500

    Avance 7

--- src/config/global.js
+++ src/config/global.js
@@ -253,7 +253,7 @@ export default {
     contenidoInstruccional: [
       {
         nombre: 'Rafael Neftalí Lizcano Reyes',
-        cargo: 'Asesor metodológico y pedagógico',
+        cargo: 'Responsable del equipo de Diseño Instruccional',
         centro:
           'Centro Industrial del Diseño y la Manufactura - Regional Santander',
       },
